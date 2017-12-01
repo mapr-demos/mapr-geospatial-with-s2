cbfffb5a8fd7fd9fd41df1b8abdaa55c52aff0a9
Vlad Storona <vstorona@cybervisiontech.com>
2017-12-01 18:06:13 +0200

    Changed data format

--- src/main/java/com/mapr/geospatial/InsertData.java
+++ src/main/java/com/mapr/geospatial/InsertData.java
@@ -2,41 +2,67 @@ package com.mapr.geospatial;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.geometry.S2LatLng;
 import com.mapr.geospatial.geo.GeoJson;
-import com.mapr.geospatial.geo.Geometry;
-import com.mapr.geospatial.geo.Properties;
+import org.ojai.Document;
+import org.ojai.store.Connection;
+import org.ojai.store.DocumentStore;
+import org.ojai.store.DriverManager;
+import org.ojai.store.Query;
+import org.ojai.store.QueryCondition;
 
-import java.util.ArrayList;
+import java.util.UUID;
 
 public class InsertData {
     public static void main(String[] args) throws JsonProcessingException {
 
-        final ObjectMapper mapper = new ObjectMapper();
+        // Create an OJAI connection to MapR cluster
+        final Connection connection = DriverManager.getConnection("ojai:mapr:");
 
+        // Get an instance of OJAI
+        final DocumentStore store = connection.getStore("/apps/geo_data");
+
+        GeoJson geo = createGeoJson();
+//        store.insert(connection.newDocument(convertToJson(geo)));
+
+        for (Document entries : store.find()) {
+            System.out.println(entries.asJsonString());
+        }
+
+        Query query = connection.newQuery()
+                .where(connection.newCondition().is("latitude", QueryCondition.Op.GREATER_OR_EQUAL, -108.01621).build())
+                .build();
+
+        GeoJson geoJson = null;
+
+        for (Document entries : store.findQuery(query)) {
+//            System.out.println(entries.asJsonString());
+            geoJson = entries.toJavaBean(GeoJson.class);
+            System.out.println(geoJson.toString());
+        }
+
+        if (geoJson != null) {
+            S2LatLng s2LatLng =
+                    S2LatLng.fromRadians(geoJson.getLatitude(), geoJson.getLongitude());
+            System.out.printf(s2LatLng.toString());
+        }
+
+        store.close();
+        connection.close();
+    }
+
+    private static String convertToJson(Object o) throws JsonProcessingException {
+        return new ObjectMapper().writeValueAsString(o);
+    }
+
+    private static GeoJson createGeoJson() {
         GeoJson geo = new GeoJson();
-        Geometry geometry = new Geometry();
-        Properties properties = new Properties();
-
-        geo.setType("Feature");
-        geometry.setType("Point");
-        geometry.setLatitude( -105.01621);
-        geometry.setLongitude(39.57422);
-        properties.setName("Dinagat Islands");
-
-        ArrayList<Geometry> geometries = new ArrayList<>();
-        geometries.add(geometry);
-
-        geo.setGeometries(geometries);
-        geo.setProperties(properties);
-
-        String value = mapper.writeValueAsString(geo);
-
-        System.out.println(value);
-
-//        final Connection connection = DriverManager.getConnection("ojai:mapr:");
-//        final DocumentStore store = connection.getStore("/demo_table");
-
-//        store.close();
-//        connection.close();
+        String newDocUUID = UUID.randomUUID().toString();
+        geo.set_id(newDocUUID);
+        geo.setType("Point");
+        geo.setLatitude(37.4185099);
+        geo.setLongitude(-121.9450038);
+        geo.setName("MapR headquarter");
+        return geo;
     }
 }
--- src/main/java/com/mapr/geospatial/geo/GeoJson.java
+++ src/main/java/com/mapr/geospatial/geo/GeoJson.java
@@ -1,12 +1,45 @@
 package com.mapr.geospatial.geo;
 
-import java.util.List;
-
 public class GeoJson {
 
+    private String _id;
+    private String name;
     private String type;
-    private List<Geometry> geometries;
-    private Properties properties;
+    private double latitude;
+    private double longitude;
+
+
+    public String get_id() {
+        return _id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
 
     public String getType() {
         return type;
@@ -16,19 +49,42 @@ public class GeoJson {
         this.type = type;
     }
 
-    public List<Geometry> getGeometries() {
-        return geometries;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GeoJson geoJson = (GeoJson) o;
+
+        if (Double.compare(geoJson.latitude, latitude) != 0) return false;
+        if (Double.compare(geoJson.longitude, longitude) != 0) return false;
+        if (_id != null ? !_id.equals(geoJson._id) : geoJson._id != null) return false;
+        if (name != null ? !name.equals(geoJson.name) : geoJson.name != null) return false;
+        return type != null ? type.equals(geoJson.type) : geoJson.type == null;
     }
 
-    public void setGeometries(List<Geometry> geometries) {
-        this.geometries = geometries;
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = _id != null ? _id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (type != null ? type.hashCode() : 0);
+        temp = Double.doubleToLongBits(latitude);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(longitude);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
     }
 
-    public Properties getProperties() {
-        return properties;
-    }
-
-    public void setProperties(Properties properties) {
-        this.properties = properties;
+    @Override
+    public String toString() {
+        return "GeoJson{" +
+                "_id='" + _id + '\'' +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
     }
 }
